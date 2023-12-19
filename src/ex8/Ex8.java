6cfd5483c32c1ccb130d03e3e6a5c7ac17f8239a
CAndreyS <alfater@gmail.com>
2023-12-19 14:41:49 +0100

    ex8 upgrade - CAS

---
+++ src/ex8/Ex8.java
@@ -0,0 +1,51 @@
+package ex8;
+
+import java.util.ArrayList;
+
+public class Ex8 {
+    public ArrayList<Integer> list;
+    public ArrayList<Integer> listT1;
+    public ArrayList<Integer> listT2;
+    public Integer n;
+    public Integer comptPairs;
+    public Integer comptImpairs;
+
+    public ArrayList<Integer> getListT1() {
+        return listT1;
+    }
+
+    public ArrayList<Integer> getListT2() {
+        return listT2;
+    }
+
+    public Integer getComptPairs() {
+        return comptPairs;
+    }
+
+    public Integer getComptImpairs() {
+        return comptImpairs;
+    }
+
+
+    public Ex8(ArrayList<Integer> list, Integer n) {
+        this.list = list;
+        this.n = n;
+    }
+
+    public void PareIparer(ArrayList<Integer> list, Integer n) {
+        comptPairs = 0;
+        comptImpairs = 0;
+        listT1 = new ArrayList<>();
+        listT2 = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i)%2 == 0) {
+                listT1.add(list.get(i));
+                comptPairs = comptPairs +1;
+            }
+            if (list.get(i)%2 != 0) {
+                listT2.add(list.get(i));
+                comptImpairs = comptImpairs + 1;
+            }
+        }
+    }
+}
