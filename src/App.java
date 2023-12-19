872c71d9bc619587fd5c5933ba384970d3211973
CAndreyS <alfater@gmail.com>
2023-12-19 11:00:22 +0100

    ex3 ready - CAS

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 import ex1.Ex1;
 import ex2.Ex2;
+import ex3.Ex3;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -10,7 +11,7 @@ public class App {
         System.out.printf("%n· %76s ·%n", " ");
 
         String col1 = "JAVA jour 2";
-        Integer widthColumn = 70;
+        Integer widthColumn = 20;
         String lineType = "─";
 
         String line = lineType.repeat(widthColumn);
@@ -38,6 +39,36 @@ public class App {
         Integer replyEx2 = ex2.CalculateSumm();
 
         System.out.printf("La somme  S=1+2+3+...+ 10: %d%n", replyEx2);
+
+        // Exercice 3
+        System.out.println("\nExercice 3\n");
+        System.out.printf("Entre nomber a: %s", "");
+        Integer a = (Integer) scanner.nextInt();
+        System.out.printf("Entre nomber b: %s", "");
+        Integer b = (Integer) scanner.nextInt();
+
+        Ex3 ex3 = new Ex3(a, b);
+        ex3.ChangerAB();
+        int aNew = ex3.getA();
+        int bNew = ex3.getB();
+        System.out.println("Inverse:");
+
+        Integer var1 = aNew;
+        Integer var2 = bNew;
+        col1 = Integer.toString(a);
+        String col2 = Integer.toString(b);
+
+        System.out.printf("┌" + space + "┬" + space + "┐%n", line, line);
+        System.out.printf("│ " + space + " │ " + space + " │%n", col1, col2);
+        System.out.printf("├" + space + "┼" + space + "┤%n", line, line);
+        
+        System.out.printf("│ " + space + " │ " + space + " │%n", var1, var2);
+        
+        System.out.printf("└" + space + "┴" + space + "┘%n", line, line);
+
+
+
+
         // End of the jour 2
         scanner.close();
 
